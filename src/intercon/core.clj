cd5dfd8ca4e2026adcc32295af5b1808be973b11
Michiel Eggermont <michiel.eggermont@gmail.com>
2012-06-17 11:31:46 +0200

    Cleanups: whitespace at end removed, comment improvements.

--- src/intercon/core.clj
+++ src/intercon/core.clj
@@ -6,12 +6,12 @@
 ;; To get a better understanding of what Cope means by DCI
 ;; I am trying to model the domain of monetary transactions
 ;; in the style of DCI. DCI results in an architecture that
-;; separates the slowly evolving parts in your system from 
+;; separates the slowly evolving parts in your system from
 ;; the fast evolving parts. This is done by the introduction
 ;; of roles and contexts.
 ;; Domain models encapsulate the slowly evolving data. These
 ;; dumb models will be assigned a role within a context to be
-;; able to execute contextual tasks. The roles therefore 
+;; able to execute contextual tasks. The roles therefore
 ;; encapsulate the ever changing business needs.
 
 ;; Of course even the dumb models should have some functionality
@@ -19,38 +19,39 @@
 ;; of an account and returns the new account with the updated balance.
 (defn modify-balance
   "Modifies the balance of the account by applying the function f to it
-   and returns the updated account"
-  [f account amount] 
-  (update-in 
+   and returns the updated account. If the balance was not found, then
+   a new balance initialized with the amount is added to the account."
+  [f account amount]
+  (update-in
     account [:balance] (fnil #(f % amount) 0)))
 
 ;; Now let's define some roles that are frequently used in the domain
 ;; of monetary transactions. On such role is that of a money sink, in
-;; which money can disappear. The other one is the money source that 
+;; which money can disappear. The other one is the money source that
 ;; can deliver money.
 (defprotocol MoneySink
   (transfer-from
     [account amount]))
 (defprotocol MoneySource
-  (transfer-to 
+  (transfer-to
     [account amount]))
 ;; Then we need a dumb domain model of an account, which is not much
-;; more than a container of account data and the mundane implementations 
+;; more than a container of account data and the mundane implementations
 ;; of account function roles of source and sink. These dumb functions are
 ;; NOT the complex, ever changing business functionality. These will come
 ;; later, when we let the dumb models play roles in a context.
 (defrecord Account [id balance ccy]
   Object
-  (toString 
+  (toString
     [this]
     (str id ": " (:balance this) " " (.toUpperCase (name (:ccy this)))))
   MoneySink
   (transfer-from
     [account amount]
-    (modify-balance + account amount)) 
-  MoneySource 
+    (modify-balance + account amount))
+  MoneySource
   (transfer-to
-    [account amount] 
+    [account amount]
     (modify-balance - account amount)))
 
 ;; As said, roles are played in a context. Here is a protocol that
@@ -60,23 +61,26 @@
   (enact
     [use-case]))
 
-;; Here is an example of a money tranfer context.
+;; Let's for example create a money tranfer context.
 ;; It can transfer amounts from one account to another using a function
 ;; for converting amounts to the ccy of the target.
-;; For this Context we also need a role for converting amounts between
-;; different currencies.
+;; For this Context we need the following roles:
+;; - MoneySource: for the source of money
+;; - MoneySink:   for the destination of money
+;; - EchangeRate: for converting amounts between different currencies.
+;; The first two were already defined. So here's the last one:
 (defprotocol ExchangeRate
   (convert [source-ccy target-ccy amount]))
 
-;; Now that we have defined the role we can create a function
+;; Now that we have identified the roles we can create a function
 ;; that will contain the business logic of transferring money
-;; from one account to another one an performing conversion 
+;; from one account to another one an performing conversion
 ;; along the way.
 (defn transfer-money
   [rate-f source target s-amt]
   (reify
     ExchangeRate
-    (convert 
+    (convert
       [context target-ccy amount]
       (let [exchange-rate (rate-f (:ccy source) target-ccy)]
         (with-precision 4 (* amount exchange-rate))))
@@ -86,23 +90,20 @@
       (let [s-ccy (:ccy source)
             t-ccy (:ccy target)
             t-amt (convert this t-ccy s-amt)]
-        {:source (transfer-to   source s-amt) 
+        {:source (transfer-to   source s-amt)
          :target (transfer-from target t-amt)}))))
 
 ;; Now we can see what happens.
 ;; Enact a use case in which we transfer 20 EUR to a NOK account.
-(let [s-act (Account. "ACC-1" 1000.00M :eur) 
+(let [s-act (Account. "ACC-1" 1000.00M :eur)
       t-act (Account. "ACC-2" 1000.00M :nok)
       s-amt 203.34M
       mt-context (transfer-money rate-fn s-act t-act s-amt)
       {:keys [source target]} (enact mt-context)]
-  (println 
-    (str "before: " s-act ", " t-act))
-  (println 
-    (str "transfer " s-amt " " (:ccy s-act)))
-  (println 
-    (str "after : " source ", " target)))
-
-
-
+  (println
+    (str "before:   " s-act ", " t-act))
+  (println
+    (str "transfer: " s-amt " " (:ccy s-act)))
+  (println
+    (str "after :   " source ", " target)))
 
--- src/intercon/rate.clj
+++ src/intercon/rate.clj
@@ -1,11 +1,11 @@
 (ns intercon.rate)
 
 (defn rate
-  "Returns the rate from s-ccy to t-ccy. 
+  "Returns the rate from s-ccy to t-ccy.
    The rate-fn is used to find the rate from a base ccy to another ccy.
    The rate returned is the ratio between t-rate and s-rate."
   [p rate-fn s-ccy t-ccy]
-  (let [s-rate (rate-fn s-ccy) 
+  (let [s-rate (rate-fn s-ccy)
         t-rate (rate-fn t-ccy)]
    (with-precision p (/ t-rate s-rate))))
 
@@ -22,24 +22,25 @@
    [p rates source target]
    (rate p #(reduce + (map (% rates) [:ask :bid])) source target))
 
-;; An example of a list of exchange rates from usd to some 
+;; An example of a list of exchange rates from usd to some
 ;; other currencies.
 (def exchange-rates
    {:usd {:ask 1M      :bid 1M     }
-    :eur {:ask 1.2003M :bid 1.2203M} 
+    :eur {:ask 1.2003M :bid 1.2203M}
     :nok {:ask 3.1232M :bid 3.3032M}
     :mxd {:ask 2.4212M :bid 2.5444M}
     :cad {:ask 1.2212M :bid 1.2444M}
     :jpy {:ask 8.9183M :bid 9.0023M}})
-(def ccys 
+(def ccys
   (keys exchange-rates))
 
 ;; Let's make a function that will be able to compute exchange rates
 ;; with a precision of 10 using mid-rates
-(def rate-fn 
+(def rate-fn
    (partial mid-rate 10 exchange-rates))
 
-;; Now print the mid exchange rates between all possible currency combinations 
+;; Now print the mid exchange rates between all possible currency
+;; combinations
 (for [s ccys t ccys]
   (let [r (rate-fn s t)]
     (println (str "1 " s  " = " r " " t) )))
